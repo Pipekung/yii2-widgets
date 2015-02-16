7ec960a4a321c39f464b917f81d6df5aae53df14
Pipekung <genius.society@gmail.com>
2015-02-16 10:52:14 +0700

    update datepicker, tableview

--- DatePicker.php
+++ DatePicker.php
@@ -2,6 +2,7 @@
 
 namespace pipekung\widgets;
 
+use Yii;
 use yii\web\View;
 use pipekung\classes\Common;
 
@@ -17,16 +18,26 @@ class DatePicker extends Widget {
     public $form;
     public $name;
     public $attr;
-    public $clientOptions = array();
-    protected $label;
-    protected $element;
+    public $options = [];
+    public $between = [];
+    protected $label = [];
+    protected $element = [];
 
     public function init() {
         parent::init();
 
-        $this->label = $this->model->getAttributeLabel($this->attr);
-        $this->element = "#". Common::getClassName($this->model) ."-{$this->attr}";
-        $this->clientOptions = array_merge($this->clientOptions, [
+        if (! empty($this->between)) {
+            $this->label[0] = $this->model->getAttributeLabel($this->between[0]);
+            $this->element[0] = "#". Common::getClassName($this->model) ."-{$this->between[0]}";
+
+            $this->label[1] = $this->model->getAttributeLabel($this->between[1]);
+            $this->element[1] = "#". Common::getClassName($this->model) ."-{$this->between[1]}";
+        } else {
+            $this->label[0] = $this->model->getAttributeLabel($this->attr);
+            $this->element[0] = "#". Common::getClassName($this->model) ."-{$this->attr}";
+        }
+
+        $this->options = array_merge($this->options, [
             'changeMonth' => true,
             'changeYear' => true,
             'hideIfNoPrevNext' => true,
@@ -37,19 +48,47 @@ class DatePicker extends Widget {
     }
 
     public function run() {
-        \yii\jui\DatePicker::widget(['model' => $this->model, 'attribute' => $this->attr]);
-        echo $this->form->field($this->model, $this->attr, [
-            'addon' => [
-                'prepend' => ['content' => '<i class="glyphicon glyphicon-calendar"></i>'],
-                'groupOptions' => ['class' => 'col-sm-4'],
-            ]
-        ])->textInput(['readonly' => 'readonly', 'placeholder' => "คลิกเลือก {$this->label}"]);
+        if (! empty($this->between)) {
+            \yii\jui\DatePicker::widget(['model' => $this->model, 'attribute' => $this->between[0]]);
+            echo $this->form->field($this->model, $this->between[0], [
+                'addon' => [
+                    'prepend' => ['content' => '<i class="glyphicon glyphicon-calendar"></i>'],
+                    'groupOptions' => ['class' => 'col-sm-4'],
+                ]
+            ])->textInput(['readonly' => 'readonly', 'placeholder' => "คลิกเลือก {$this->label[0]}"]);
+
+            \yii\jui\DatePicker::widget(['model' => $this->model, 'attribute' => $this->between[1]]);
+            echo $this->form->field($this->model, $this->between[1], [
+                'addon' => [
+                    'prepend' => ['content' => '<i class="glyphicon glyphicon-calendar"></i>'],
+                    'groupOptions' => ['class' => 'col-sm-4'],
+                ]
+            ])->textInput(['readonly' => 'readonly', 'placeholder' => "คลิกเลือก {$this->label[1]}"]);
+        } else {
+            \yii\jui\DatePicker::widget(['model' => $this->model, 'attribute' => $this->attr]);
+            echo $this->form->field($this->model, $this->attr, [
+                'addon' => [
+                    'prepend' => ['content' => '<i class="glyphicon glyphicon-calendar"></i>'],
+                    'groupOptions' => ['class' => 'col-sm-4'],
+                ]
+            ])->textInput(['readonly' => 'readonly', 'placeholder' => "คลิกเลือก {$this->label[0]}"]);
+        }
         $this->js();
     }
 
     public function js() {
-        $options = json_encode($this->clientOptions);
-        \Yii::$app->view->registerJs("\$('{$this->element}').datepicker({$options});", View::POS_END);
+        $options = json_encode($this->options);
+        if (! empty($this->between)) {
+            Yii::$app->view->registerJs("
+                $('{$this->element[0]}').datepicker({$options}); 
+                $('{$this->element[1]}').datepicker({$options});
+                $('{$this->element[0]}').datepicker('option', 'onSelect', function( selectedDate ) { $('{$this->element[1]}').datepicker('option', 'minDate', selectedDate); calculateDate(); });
+                $('{$this->element[1]}').datepicker('option', 'onSelect', function( selectedDate ) { $('{$this->element[0]}').datepicker('option', 'maxDate', selectedDate); calculateDate(); });
+            ", View::POS_END);
+        } else {
+            Yii::$app->view->registerJs("$('{$this->element[0]}').datepicker({$options});", View::POS_END);
+            Yii::$app->view->registerJs("$('{$this->element[0]}').datepicker('option', 'onSelect', function() { calculateDate(); });", View::POS_END);
+        }
     }
 
 }
--- TableView.php
+++ TableView.php
@@ -2,6 +2,9 @@
 
 namespace pipekung\widgets;
 
+use Yii;
+use yii\helpers\Url;
+
 /**
  * Description of TableView
  *
@@ -12,10 +15,32 @@ class TableView extends Widget {
 
     public $data;
     public $column = [];
+    public $id;
     public $class = 'table table-bordered table-striped table-hover';
+    public $action;
 
     public function init() {
         parent::init();
+
+        if (isset($this->action)) {
+            if (! isset($this->action['headerStyle'])) {
+                $this->action['headerStyle'] = 'width: 120px;';
+            }
+
+            if (! isset($this->action['style'])) {
+                $this->action['style'] = 'text-align: center;';
+            }
+        } else {
+            $this->action = [
+                'template' => [
+                    'view' => '<a class="btn btn-xs btn-default" href=""><i class="glyphicon glyphicon-th-list"></i></a>',
+                    'update' => '<a class="btn btn-xs btn-primary" href=""><i class="glyphicon glyphicon-pencil"></i></a>',
+                    'delete' => '<button type="button" class="btn btn-xs btn-danger"><i class="glyphicon glyphicon-trash"></i></button>',
+                ],
+                'headerStyle' => 'width: 120px;',
+                'style' => 'text-align: center;',
+            ];
+        }
     }
 
     public function run() {
@@ -23,7 +48,7 @@ class TableView extends Widget {
         foreach ($this->column as $val) {
             $TbHeader .= "<th>{$val['header']}</th>";
         }
-        $TbHeader .= '<th style="width: 80px;"></th>';
+        $TbHeader .= "<th style='{$this->action['headerStyle']}'></th>";
 
         $TbData = '';
         foreach ($this->data as $val) {
@@ -31,23 +56,34 @@ class TableView extends Widget {
             foreach ($this->column as $val2) {
                 $TbData .= "<td>{$val[$val2['value']]}</td>";
             }
-            $TbData .= '<td style="text-align: center;">
-                <a class="btn btn-xs btn-primary" href=""><i class="glyphicon glyphicon-pencil"></i></a> 
-                <button type="button" class="btn btn-xs btn-danger"><i class="glyphicon glyphicon-trash"></i></button>
-            </td>';
-            $TbData .= '</tr>';
-        }
+            $TbData .= "<td style='{$this->action['style']}'>";
+            if (in_array('view', $this->action['template']) || isset($this->action['template']['view'])) {
+                if (is_array($this->action['template']['view'])) 
+                    $TbData .= '<a class="btn btn-xs btn-default" href="{{url-to-view}}"><i class="glyphicon glyphicon-th-list"></i></a>';
+                else 
+                    $TbData .= '<a class="btn btn-xs btn-default" href="{{url-to-view}}"><i class="glyphicon glyphicon-th-list"></i></a>';
+            } else $TbData .= '';
 
-        echo <<< HTML
-<table class="{$this->class}">
-    <thead>
-        {$TbHeader}
-    </thead>
-    <tbody>
-        {$TbData}
-    </tbody>
-</table>
-HTML;
+            if (in_array('update', $this->action['template']) || isset($this->action['template']['update'])) {
+                if (is_array($this->action['template']['update'])) 
+                    $TbData .= '<a class="btn btn-xs btn-primary" href="'. Url::to([$this->action['template']['update']['url'], 'id' => $val['id']]) .'"><i class="glyphicon glyphicon-pencil"></i></a>';
+                else 
+                    $TbData .= '<a class="btn btn-xs btn-primary" href="'. Url::to(['update', 'id' => $val['id']]) .'"><i class="glyphicon glyphicon-pencil"></i></a>';
+            } else $TbData .= '';
+
+            if (in_array('delete', $this->action['template']) || isset($this->action['template']['delete'])) {
+                if (is_array($this->action['template']['delete'])) 
+                    $TbData .= '<button type="button" class="btn btn-xs btn-danger"><i class="glyphicon glyphicon-trash"></i></button>';
+                else 
+                    $TbData .= '<button type="button" class="btn btn-xs btn-danger"><i class="glyphicon glyphicon-trash"></i></button>';
+            } else $TbData .= '';
+
+            $TbData .= '</td></tr>';
+        }
+        echo "<table class='{$this->class}' id='{$this->id}'>
+            <thead>{$TbHeader}</thead>
+            <tbody>{$TbData}</tbody>
+        </table>";
     }
 
 }
